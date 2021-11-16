10aa61ea084e1d280af12133c7c1ff148fbc88bd
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-11-16 15:39:37 +0100

    Day 19 completed

---
+++ 2015/Day19/tasks.cpp
@@ -0,0 +1,78 @@
+
+#include "../../readFile.h"
+#include "../../utilities.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <numeric>
+
+auto parseInput = [](const auto& input){
+    std::vector<std::pair<std::string, std::string>> replacements;
+    for(const auto& s : input){
+        if(s.empty()) break;
+        auto split = Utilities::split(s);
+        replacements.emplace_back(std::move(split[0]), std::move(split[2]));
+    }
+
+    return std::make_pair(replacements, input.back());
+};
+
+auto iterateMolecules = [](auto& molecules, const auto& replacements){
+    auto addNewMolecules = [](const auto& molecule, auto& newMolecules, const auto& fromReplacement, const auto& toReplacement){
+        const auto toReplaceRanges = Utilities::searchAll(molecule, fromReplacement);
+        for(const auto& subrange : toReplaceRanges){
+            auto first = std::distance(std::begin(molecule), subrange.begin());
+            auto last = std::distance(std::begin(molecule), subrange.end());
+            newMolecules.emplace(molecule.substr(0, first) + toReplacement + molecule.substr(last));
+        }
+    };
+
+    std::unordered_set<std::string> newMolecules{};
+    for(const auto& molecule : molecules){
+        for(const auto& [fromReplacement, toReplacement] : replacements){
+            addNewMolecules(molecule, newMolecules, fromReplacement, toReplacement);
+        }
+    }
+    std::swap(molecules, newMolecules);
+};
+
+
+auto countDistinctMolecules = [](const auto& molecule, const auto& replacements){
+
+    std::unordered_set<std::string> molecules{molecule};
+    iterateMolecules(molecules, replacements);
+
+    return molecules.size();
+};
+
+
+auto countStepsToCreateMolecule = [](auto& goalMolecule, auto& replacements){
+    std::unordered_set<std::string> elements{"CRn"};
+    for(const auto& [fromReplacement, toReplacement] : replacements){
+        elements.insert(fromReplacement);
+    }
+
+    auto getElementCount = [&goalMolecule](const auto& element){
+        return Utilities::searchAll(goalMolecule, element).size();
+    };
+
+    auto countElements = std::transform_reduce(std::begin(elements), std::end(elements),
+                                               0u, std::plus<>(), getElementCount);
+    auto countY = std::ranges::count(goalMolecule, 'Y');
+    return countElements-countY-1;
+};
+
+int main(){
+    const auto input = readFile::vectorOfStrings("input.txt");
+
+    auto [replacements, molecule] = parseInput(input);
+
+    const auto distinctMolecules = countDistinctMolecules(molecule, replacements);
+    std::cout << "There are " << distinctMolecules << " distinct molecules that can be created.\n";
+
+    const auto stepsToCreateMolecule = countStepsToCreateMolecule(molecule, replacements);
+    std::cout << "It will take at least " << stepsToCreateMolecule << " steps to create the desired molecule.\n";
+}
