6b6b8988c5a8c33b470fc385396a37dd01ca4e24
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-11-22 19:31:36 +0100

    Day 6 completed

---
+++ 2016/Day06/tasks.cpp
@@ -0,0 +1,39 @@
+
+#include "../../readFile.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+auto getCorrectLetter = [](const auto& messages, const auto i, const auto& cmp){
+    std::unordered_map<char, unsigned int> characterQuantity{};
+    for(const auto& message : messages){
+        characterQuantity[message[i]]++;
+    }
+
+    return std::ranges::max_element(characterQuantity,
+                [&cmp](const auto& lhs, const auto& rhs){return cmp(lhs.second,rhs.second);}
+           )->first;
+};
+
+auto getCorrectedMessage = []<typename comparator = std::less<>>
+                           (const auto& messages, const comparator& cmp = std::less<>()){
+    std::string message{};
+    for(auto i=0u; i<messages[0].size(); i++){
+        message+=getCorrectLetter(messages, i, cmp);
+    }
+    return message;
+};
+
+int main(){
+    const auto messages = readFile::vectorOfStrings("input.txt");
+
+    //Task 1
+    const auto correctedMessage = getCorrectedMessage(messages);
+    std::cout << "The corrected message says: " << correctedMessage << ".\n";
+
+    //Task 2
+    const auto originalMessage = getCorrectedMessage(messages, std::greater_equal<>());
+    std::cout << "The original message says: " << originalMessage << ".\n";
+}
