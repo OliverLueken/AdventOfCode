b4cade12afebe2cfe1413ba6541e1c812b49c88f
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-02 11:40:27 +0100

    Exchanging for-loops with std algorithms

--- 2016/Day04/tasks.cpp
+++ 2016/Day04/tasks.cpp
@@ -22,7 +22,7 @@ public:
         name = input.substr(0, std::distance(std::begin(input), lastDash));
         auto split = Utilities::split(lastDash+1, std::end(input), '[');
         id = std::stoi(split[0]);
-        split[1].pop_back();
+        split[1].pop_back(); //removes the trailing ']'
         checksum = std::move(split[1]);
     }
 
@@ -33,65 +33,68 @@ public:
         for(const auto c : name) quantityMap[c]++;
 
         std::vector<std::pair<char, int>> quantityVec{};
-        for(const auto [c, n] : quantityMap){
-            quantityVec.emplace_back(std::make_pair(c, n));
-        }
+        std::ranges::move(std::begin(quantityMap),
+                          std::end(quantityMap),
+                          std::back_inserter(quantityVec)
+        );
 
-        std::ranges::sort(quantityVec, [](const auto& lhs, const auto& rhs){
-            const auto [c1, n1] = lhs;
-            const auto [c2, n2] = rhs;
-            if(n1==n2) return c1<c2;
-            return n1 > n2;
-        });
+        std::ranges::sort(quantityVec,
+            [](const auto& lhs, const auto& rhs){
+                const auto [c1, n1] = lhs;
+                const auto [c2, n2] = rhs;
+                if(n1==n2) return c1<c2;
+                return n1 > n2;
+            }
+        );
 
-        bool isValid = true;
-        for(int letter=0; letter<5; letter++){
-            isValid &= quantityVec[letter].first == checksum[letter];
-        }
-        return isValid;
+        const auto matchedUntilIt = std::ranges::mismatch(quantityVec, checksum, {},
+            [](const auto& quantity){ return quantity.first; }
+        ).in2;
+        return matchedUntilIt == std::end(checksum);
     }
 
     std::string decrypt() const{
-        auto rot = id%26;
+        const auto rot = id%26;
         std::string decryptedName{};
-        for(const auto c : name){
-            if(c == '-') decryptedName+=' ';
-            else decryptedName += (char) (c+rot-'a')%26 + 'a';
-        }
+        std::ranges::transform(name, std::back_inserter(decryptedName),
+            [rot](const auto c) -> char {
+                return c == '-' ? ' ' : (char) (c+rot-'a')%26 + 'a';
+            }
+        );
         return decryptedName;
     }
 };
 
-auto parseInput = [](auto input){
+auto parseInput = [](auto&& input){
     std::vector<Room> rooms{};
-    for(auto& roomData : input){
-        Room room{};
-        room.parseInput(std::move(roomData));
-        rooms.emplace_back(std::move(room));
-    }
+    std::ranges::transform(input, std::back_inserter(rooms),
+        [](auto& roomData){
+            Room room{};
+            room.parseInput(std::move(roomData));
+            return room;
+        }
+    );
     return rooms;
 };
 
-auto sumValidroomIDs = [](const auto& rooms){
-    return std::transform_reduce(std::begin(rooms), std::end(rooms),
-                                 0u, std::plus<>(),
-                                 [](const auto& room){return room.isValid() ? room.getID() : 0u;});
+auto sumValidRoomIDs = [](const auto& rooms){
+    return std::transform_reduce(std::begin(rooms), std::end(rooms), 0u, std::plus<>(),
+        [](const auto& room){return room.isValid() ? room.getID() : 0u;}
+    );
 };
 
 auto getNorthPoleObjectStorageID = [](const auto& rooms){
-
-    for(const auto& room : rooms){
-        if(room.decrypt() == "northpole object storage")
-            return room.getID();
-    }
-    return 0u;
+    const auto storageIt = std::ranges::find(rooms, "northpole object storage",
+        [](const auto& room){return room.decrypt();}
+    );
+    return storageIt->getID();
 };
 
 int main(){
     const std::vector<Room> rooms = parseInput(readFile::vectorOfStrings("input.txt"));
 
     //Task 1
-    const auto sumOfValidRoomIDs = sumValidroomIDs(rooms);
+    const auto sumOfValidRoomIDs = sumValidRoomIDs(rooms);
     std::cout << "The sum of all valid room IDs is " << sumOfValidRoomIDs << ".\n";
 
     //Task 2
