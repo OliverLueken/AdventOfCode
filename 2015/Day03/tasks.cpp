23ba07f7636ae3ebff42478f2410052eec87393a
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-03 15:29:27 +0100

    Refactoring

--- 2015/Day03/tasks.cpp
+++ 2015/Day03/tasks.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <unordered_set>
 #include <iostream>
+#include <ranges>
+#include <string>
 
 
 using Position = std::pair<int,int>;
@@ -13,58 +15,64 @@ struct positionHash{
     }
 };
 
-int main(){
-    const auto input = readFile::string("input.txt");
+auto deliverPresents = [](auto&& instructions, auto& housesWithPresents) {
+    auto position = std::make_pair(0,0);
 
-    std::unordered_set<Position, positionHash> housesWithPresents;
-
-    auto deliverPresents = [&housesWithPresents](auto&& range) {
-        auto position = std::make_pair(0,0);
-
-        auto move = [&position](char direction){
-            switch(direction){
-                case '>':
-                    position.first++;
-                    break;
-                case '<':
-                    position.first--;
-                    break;
-                case '^':
-                    position.second++;
-                    break;
-                case 'v':
-                    position.second--;
-                    break;
-            }
-        };
-
-        auto moveAndDeliver = [&position, &housesWithPresents, &move](char direction){
-            move(direction);
-            housesWithPresents.insert(position);
-        };
-
-        housesWithPresents.insert(position); //Deliver to first house (0,0) before moving
-        std::ranges::for_each(range, moveAndDeliver);
+    auto move = [&position](const char direction){
+        switch(direction){
+            case '>':
+                position.first++;
+                break;
+            case '<':
+                position.first--;
+                break;
+            case '^':
+                position.second++;
+                break;
+            case 'v':
+                position.second--;
+                break;
+        }
     };
 
+    auto moveAndDeliver = [&position, &housesWithPresents, &move](const char direction){
+        move(direction);
+        housesWithPresents.insert(position);
+    };
 
-    //Task 1
-    deliverPresents(input);
-    std::cout << housesWithPresents.size() << " houses receive at least one present.\n";
+    housesWithPresents.insert(position); //Deliver to first house (0,0) before moving
+    std::ranges::for_each(instructions, moveAndDeliver);
+};
 
 
-    //Task 2
+auto deliverWithSanta(const auto& input){
+    std::unordered_set<Position, positionHash> housesWithPresents{};
+    deliverPresents(input, housesWithPresents);
+    return housesWithPresents.size();
+}
+
+auto deliverWithSantaAndRobot(const auto& input){
+    std::unordered_set<Position, positionHash> housesWithPresents{};
+
     auto even = [i=0](const auto&) mutable {return 0 == i++%2;};
     auto odd  = [i=1](const auto&) mutable {return 0 == i++%2;};
 
-    housesWithPresents.clear();
+    deliverPresents(input | std::views::filter(even), housesWithPresents); //Deliver with Santa
+    deliverPresents(input | std::views::filter(odd) , housesWithPresents); //Deliver with Robo-Santa
 
-    //Deliver with Santa
-    deliverPresents(input | std::views::filter(even));
+    return housesWithPresents.size();
+}
 
-    //Deliver with Robo-Santa
-    deliverPresents(input | std::views::filter(odd));
+int main(){
+    const auto input = readFile::string("input.txt");
 
-    std::cout << "With the help of Robo-Santa, " << housesWithPresents.size() << " houses receive at least one present.\n";
+    //Task 1
+    const auto numberOfHousesWithPresents = deliverWithSanta(input);
+    std::cout << "Santa delivered presents to " << numberOfHousesWithPresents << " different houses.\n";
+
+
+    //Task 2
+    const auto numberOfHousesWithPresents2 = deliverWithSantaAndRobot(input);
+    std::cout << "With the help of Robo-Santa, " << numberOfHousesWithPresents2 << " houses receive at least one present.\n";
 
 }
