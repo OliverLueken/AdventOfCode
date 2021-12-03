4615908cd63a1e8d4450a6d7fabf0e949f54c0e0
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-03 14:59:22 +0100

    cleanup vectorOfVectorOfInts

--- lib/readFile.hpp
+++ lib/readFile.hpp
@@ -9,6 +9,8 @@
 #include <limits.h>
 #include <ranges>
 
+#include "utilities.hpp"
+
 namespace readFile{
 
     auto getStream(const std::string& filename){
@@ -30,28 +32,19 @@ namespace readFile{
     };
 
     auto vectorOfVectorOfInts(const std::string& filename, char del1 = '\n', char del2 = ' '){
-        auto text{getStream(filename).str()};
+        const auto text{getStream(filename).str()};
 
+        const auto rows = Utilities::split(text, del1);
         std::vector<std::vector<int>> v{};
-
-        auto addToVec = [&v, &del2](auto left, const auto& right){
-            std::vector<int> innerVec{};
-            while(left < right){
-                auto delimiterIt = std::find(left, right, del2);
-                const std::string tempNum{left, delimiterIt};
-                innerVec.push_back(std::stoi(tempNum));
-                left = delimiterIt+1;
+        std::ranges::for_each(
+            rows,
+            [&v, del2](auto& row){
+                const auto split = Utilities::split(row, del2);
+                std::vector<int> elements{};
+                std::ranges::transform(split, std::back_inserter(elements), [](const auto& s){ return std::stoi(s);} );
+                v.push_back( std::move(elements) );
             }
-            v.push_back(innerVec);
-        };
-
-        auto left = std::begin(text);
-        const auto right = std::end(text);
-        while(left < right){
-            auto delimiterIt = std::find(left, right, del1);
-            addToVec(left, delimiterIt);
-            left = delimiterIt+1;
-        }
+        );
         return v;
     }
 
