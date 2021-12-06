2a09c2a345c57973bbafa344ba33c32e1d3ddf14
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-06 11:43:34 +0100

    refactoring

--- 2015/Day06/tasks.cpp
+++ 2015/Day06/tasks.cpp
@@ -1,97 +1,104 @@
 
 #include "../../lib/readFile.hpp"
+#include "../../lib/utilities.hpp"
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <ctype.h>
-#include <numeric>
 
-struct instruction{
-    std::string instr;
+using Command = std::function<void(int&)>;
+
+class Commands{
+public:
+    Command turnOn{};
+    Command turnOff{};
+    Command toggle{};
+};
+
+struct Instruction{
+    Command* commandPtr;
     unsigned int x_start;
     unsigned int y_start;
     unsigned int x_end;
     unsigned int y_end;
 };
 
-auto parseInput = [](const auto& input){
 
-    auto parse = [](const auto& string){
-        instruction instruction{};
-        auto pos = string.find_first_of("0123456789");
-        instruction.instr = string.substr(0,pos-1); //-1 gets rid of the trailing white space
-        instruction.x_start = std::stoi(string.substr(pos));
-        pos = string.find(',', pos);
-        instruction.y_start = std::stoi(string.substr(pos+1));
-        pos = string.find(' ', pos+5);
-        instruction.x_end = std::stoi(string.substr(pos+1));
-        pos = string.find(',', pos+1);
-        instruction.y_end = std::stoi(string.substr(pos+1));
+auto parseInput = [](const auto& input, Commands& commands){
+
+    auto parse = [&commands](const auto& string){
+        Instruction instruction{};
+        const auto split = Utilities::splitOnEach(string, " ,l");
+
+        if(split[1] == "on"){
+            instruction.commandPtr = &commands.turnOn;
+        }
+        else if(split[1] == "off"){
+            instruction.commandPtr = &commands.turnOff;
+        }
+        else{ //toggle
+            instruction.commandPtr = &commands.toggle;
+        }
+
+        instruction.x_start = std::stoi(split[2]);
+        instruction.y_start = std::stoi(split[3]);
+        instruction.x_end   = std::stoi(split[5]);
+        instruction.y_end   = std::stoi(split[6]);
 
         return instruction;
     };
 
-    std::vector<instruction> instructions;
+    std::vector<Instruction> instructions;
     std::ranges::transform(input, std::back_inserter(instructions), parse);
     return instructions;
 };
 
 
-auto followInstructions = [](auto& lights, const auto& instructions, const auto& TurnOn, const auto& TurnOff, const auto& Toggle){
-    auto followInstruction = [&lights, &TurnOn, &TurnOff, &Toggle](const auto& instruction){
+auto getBrightness = [](const auto& instructions){
 
-        auto makeLights = [&lights, &instruction](const auto& instr){
-            for(auto y=instruction.y_start; y<=instruction.y_end; y++){
-                for(auto x=instruction.x_start; x<=instruction.x_end; x++){
-                    instr(lights[x+y*1000]);
-                }
+    std::array<int, 1000*1000> lights{};
+
+    auto followInstruction = [&lights](const auto& instruction){
+        auto& command = *instruction.commandPtr;
+        for(auto y=instruction.y_start; y<=instruction.y_end; y++){
+            for(auto x=instruction.x_start; x<=instruction.x_end; x++){
+                command(lights[x+y*1000]);
             }
-        };
-
-        if(instruction.instr == "turn on"){
-            makeLights(TurnOn);
-        }
-        else if(instruction.instr == "turn off"){
-            makeLights(TurnOff);
-        }
-        else{
-            makeLights(Toggle);
         }
     };
 
     std::ranges::for_each(instructions, followInstruction);
+    return Utilities::sum(lights);
 };
 
 
+auto getTaskOneCommands(){
+    auto turnOn  = [](auto& light){light=1;};
+    auto turnOff = [](auto& light){light=0;};
+    auto toggle  = [](auto& light){light=1^light;};
+    return Commands(turnOn, turnOff, toggle);
+}
+
+auto getTaskTwoCommands(){
+    auto turnOn  = [](auto& light){light++;};
+    auto turnOff = [](auto& light){if(light>0) light--;};
+    auto toggle  = [](auto& light){light+=2;};
+    return Commands(turnOn, turnOff, toggle);
+}
+
+
 int main(){
-    const auto input = readFile::vectorOfStrings("input.txt");
+    Commands commands{};
+    const auto instructions = parseInput(readFile::vectorOfStrings("input.txt"), commands);
 
-    const auto instructions = parseInput(input);
-
-    {  //Task 1
-        std::array<bool, 1000*1000> lights{};
-
-        auto TurnOn  = [](auto& light){light=true;};
-        auto TurnOff = [](auto& light){light=false;};
-        auto Toggle  = [](auto& light){light=!light;};
-
-        followInstructions(lights, instructions, TurnOn, TurnOff, Toggle);
-
-        auto lightsOn = std::accumulate(std::begin(lights), std::end(lights), 0u);
-        std::cout << "There are a total of " << lightsOn << " lights lit now.\n";
-    }
+    //Task 1
+    commands = getTaskOneCommands();
+    const auto brightnessTaskOne = getBrightness(instructions);
+    std::cout << "There are a total of " << brightnessTaskOne << " lights lit now.\n";
 
     //Task 2
-    std::array<unsigned int, 1000*1000> lights{};
-
-    auto TurnOn  = [](auto& light){light++;};
-    auto TurnOff = [](auto& light){if(light>0) light--;};
-    auto Toggle  = [](auto& light){light+=2;};
-
-    followInstructions(lights, instructions, TurnOn, TurnOff, Toggle);
-
-    auto brightness = std::accumulate(std::begin(lights), std::end(lights), 0u);
-    std::cout << "The total brightness of the lights combine to " << brightness << ".\n";
+    commands = getTaskTwoCommands();
+    const auto brightnessTaskTwo = getBrightness(instructions);
+    std::cout << "The total brightness of the lights combine to " << brightnessTaskTwo << ".\n";
 }
