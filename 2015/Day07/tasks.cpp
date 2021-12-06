79b993e6fe4b9be1a34bf9d839a9c5d65a08db08
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-06 16:56:32 +0100

    Using variant instead of polymorphism

--- 2015/Day07/tasks.cpp
+++ 2015/Day07/tasks.cpp
@@ -6,186 +6,218 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cstdint>
-#include <memory>
+#include <variant>
 
-class gate{
-protected:
-    std::unordered_map<std::string, uint16_t>& signals;
+
+
+class andGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
     std::string outWire{};
-
-public:
-    gate(std::unordered_map<std::string, uint16_t>& sig) : signals{sig}{}
-    gate(std::unordered_map<std::string, uint16_t>& sig, std::string&& out)
-        : signals{sig}, outWire{std::move(out)}{}
-    virtual void passSignal() = 0;
-    virtual bool isReady() = 0;
-};
-
-class andGate : public gate{
-public:
     std::string wire1{};
     std::string wire2{};
 
-    andGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in1, std::string in2)
-        : gate{signals, std::move(out)}, wire1{std::move(in1)}, wire2{std::move(in2)}{
-            if(std::isdigit(wire1[0])) signals[wire1] = (uint16_t)std::stoul(wire1);
-            if(std::isdigit(wire2[0])) signals[wire2] = (uint16_t)std::stoul(wire2);
-        }
-    void passSignal(){
-        signals[outWire] = signals.at(wire1)&signals.at(wire2);
+public:
+    andGate(std::unordered_map<std::string, uint16_t>* signals, std::string&& out, std::string&& in1, std::string&& in2)
+        : signals{signals}, outWire{std::move(out)}, wire1{std::move(in1)}, wire2{std::move(in2)}{}
+    void passSignal() const {
+        (*signals)[outWire] = (*signals)[wire1]&(*signals)[wire2];
     }
-    bool isReady(){
-        return signals.contains(wire1) && signals.contains(wire2);
+    bool isReady() const {
+        auto isready = signals->contains(wire1) && signals->contains(wire2);
+        return isready;
     }
 };
 
-class orGate : public gate{
-public:
+class orGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
+    std::string outWire{};
     std::string wire1{};
     std::string wire2{};
 
-    orGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in1, std::string in2)
-        : gate{signals, std::move(out)}, wire1{std::move(in1)}, wire2{std::move(in2)}{
-            if(std::isdigit(wire1[0])) signals[wire1] = (uint16_t)std::stoul(wire1);
-            if(std::isdigit(wire2[0])) signals[wire2] = (uint16_t)std::stoul(wire2);
-        }
-    void passSignal(){
-        signals[outWire] = signals.at(wire1)|signals.at(wire2);
+public:
+    orGate(std::unordered_map<std::string, uint16_t>* signals, std::string&& out, std::string&& in1, std::string&& in2)
+        : signals{signals}, outWire{std::move(out)}, wire1{std::move(in1)}, wire2{std::move(in2)}{}
+    void passSignal() const {
+        (*signals)[outWire] = (*signals)[wire1]|(*signals)[wire2];
     }
-    bool isReady(){
-        return signals.contains(wire1) && signals.contains(wire2);
+    bool isReady() const {
+        return signals->contains(wire1) && signals->contains(wire2);
     }
 };
 
-class lshiftGate : public gate{
-public:
+class lshiftGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
+    std::string outWire{};
     std::string wire{};
     size_t shift{};
 
-    lshiftGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in1, std::string in2)
-        : gate{signals, std::move(out)}, wire{std::move(in1)}, shift{std::stoul(in2)} {
-            if(std::isdigit(wire[0])) signals[wire] = (uint16_t)std::stoul(wire);
-        }
-    void passSignal(){
-        signals[outWire] = signals.at(wire)<<shift;
+public:
+    lshiftGate(std::unordered_map<std::string, uint16_t>* signals, std::string&& out, std::string&& in1, std::string&& in2)
+        : signals{signals}, outWire{std::move(out)}, wire{std::move(in1)}, shift{std::stoul(in2)} {}
+    void passSignal() const {
+        (*signals)[outWire] = (*signals)[wire]<<shift;
     }
-    bool isReady(){
-        return signals.contains(wire);
+    bool isReady() const {
+        return signals->contains(wire);
     }
 };
 
-class rshiftGate : public gate{
-public:
+class rshiftGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
+    std::string outWire{};
     std::string wire{};
     size_t shift{};
 
-    rshiftGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in1, std::string in2)
-        : gate{signals, std::move(out)}, wire{std::move(in1)}, shift{std::stoul(in2)} {
-            if(std::isdigit(wire[0])) signals[wire] = (uint16_t)std::stoul(wire);
-        }
-    void passSignal(){
-        signals[outWire] = signals.at(wire)>>shift;
+public:
+    rshiftGate(std::unordered_map<std::string, uint16_t>* signals, std::string&& out, std::string&& in1, std::string&& in2)
+        : signals{signals}, outWire{std::move(out)}, wire{std::move(in1)}, shift{std::stoul(in2)} {}
+    void passSignal() const {
+        (*signals)[outWire] = (*signals)[wire]>>shift;
     }
-    bool isReady(){
-        return signals.contains(wire);
+    bool isReady() const {
+        return signals->contains(wire);
     }
 };
 
-class notGate : public gate{
-public:
+class notGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
+    std::string outWire{};
     std::string wire{};
 
-    notGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in)
-        : gate{signals, std::move(out)}, wire{std::move(in)}{
-            if(std::isdigit(wire[0])) signals[wire] = (uint16_t)std::stoul(wire);
-        }
-    void passSignal(){
-        signals[outWire] = ~signals.at(wire);
-    }
-    bool isReady(){
-        return signals.contains(wire);
-    }
-};
-
-class defaultGate : public gate{
 public:
-    std::string wire{};
-
-    defaultGate(std::unordered_map<std::string, uint16_t>& signals, std::string out, std::string in)
-        : gate{signals, std::move(out)}, wire{std::move(in)}{
-            if(std::isdigit(wire[0])) signals[wire] = (uint16_t)std::stoul(wire);
-        }
-    void passSignal(){
-        signals[outWire] = signals.at(wire);
+    notGate(std::unordered_map<std::string, uint16_t>* signals, std::string&& out, std::string&& in)
+        : signals{signals}, outWire{std::move(out)}, wire{std::move(in)}{}
+    void passSignal() const {
+        (*signals)[outWire] = ~(*signals)[wire];
     }
-    bool isReady(){
-        return signals.contains(wire);
+    bool isReady() const {
+        return signals->contains(wire);
     }
 };
 
+class defaultGate{
+    std::unordered_map<std::string, uint16_t>* signals{nullptr};
+    std::string outWire{};
+    std::string inWire{};
 
-auto parseInput = [](const auto& input, auto& signals){
-    auto parse = [&signals](const auto& input) -> std::unique_ptr<gate>{
-        auto parts = Utilities::split(input);
-
-        std::string out = *std::rbegin(parts);
-        if(parts[0] == "NOT"){
-            return std::make_unique<notGate>(signals, out, parts[1]);
-        }
-        else if(parts[1] == "->"){
-            return std::make_unique<defaultGate>(signals, out, parts[0]);
-        }
-        else if(parts[1] == "AND"){
-            return std::make_unique<andGate>(signals, out, parts[0], parts[2]);
-        }
-        else if(parts[1] == "OR"){
-            return std::make_unique<orGate>(signals, out, parts[0], parts[2]);
-        }
-        else if(parts[1] == "LSHIFT"){
-            return std::make_unique<lshiftGate>(signals, out, parts[0], parts[2]);
-        }
-        return std::make_unique<rshiftGate>(signals, out, parts[0], parts[2]);
-
-    };
-
-    std::vector<std::unique_ptr<gate>> gates;
-    std::ranges::transform(input, std::back_inserter(gates), parse);
-    return gates;
-};
-
-auto propagateSignals = [](auto& signals, auto& gates){
-    auto signalPassedThrough = [&signals](auto& gatePtr){
-        if(gatePtr->isReady()){
-            gatePtr->passSignal();
-            return true;
-        }
-        return false;
-    };
-
-    while(!gates.empty())
-    {
-        std::erase_if(gates,signalPassedThrough);
+public:
+    defaultGate(std::unordered_map<std::string, uint16_t>* signals, auto&& out, auto&& in)
+        : signals{signals}, outWire{std::move(out)}, inWire{std::move(in)}{}
+    void passSignal() const {
+        (*signals)[outWire] = (*signals)[inWire];
+    }
+    bool isReady() const {
+        return signals->contains(inWire);
     }
 };
+
+
+class Circuit{
+    std::unordered_map<std::string, uint16_t> signals{};
+    std::unordered_map<std::string, uint16_t> presetSignals{};
+    std::vector<std::variant<andGate, orGate, lshiftGate, rshiftGate, notGate, defaultGate>> gates{};
+
+public:
+    void createGates(const auto& input){
+
+        for(const auto& inputGate : input){
+            auto parts = Utilities::split(inputGate);
+            std::string& out = parts[parts.size()-1];
+            if(parts[0] == "NOT"){
+                gates.emplace_back(notGate(&signals, std::move(out), std::move(parts[1]) ));
+            }
+            else if(parts[1] == "->"){
+                auto& inWire = parts[0];
+                if( std::isdigit(inWire[0]) ){
+                    presetSignals[out] = (uint16_t) std::stoul( inWire );
+                    continue;
+                }
+                else{
+                    gates.emplace_back(defaultGate(&signals, std::move(out), std::move(inWire) ));
+                }
+
+            }
+            else if(parts[1] == "AND"){
+                auto& inWire1 = parts[0];
+                auto& inWire2 = parts[2];
+                if(std::isdigit(inWire1[0])) presetSignals[inWire1] = (uint16_t) std::stoul(inWire1);
+                if(std::isdigit(inWire2[0])) presetSignals[inWire2] = (uint16_t) std::stoul(inWire2);
+                gates.emplace_back(andGate(&signals, std::move(out), std::move(inWire1), std::move(inWire2) ));
+            }
+            else if(parts[1] == "OR"){
+                auto& inWire1 = parts[0];
+                auto& inWire2 = parts[2];
+                if(std::isdigit(inWire1[0])) presetSignals[inWire1] = (uint16_t) std::stoul(inWire1);
+                if(std::isdigit(inWire2[0])) presetSignals[inWire2] = (uint16_t) std::stoul(inWire2);
+                gates.emplace_back(orGate(&signals, std::move(out), std::move(inWire1), std::move(inWire2) ));
+            }
+            else if(parts[1] == "LSHIFT"){
+                auto& inWire = parts[0];
+                auto& shift = parts[2];
+                if(std::isdigit(inWire[0])) presetSignals[inWire] = (uint16_t)std::stoul(inWire);
+                gates.emplace_back(lshiftGate(&signals, std::move(out), std::move(inWire), std::move(shift) ));
+            }
+            else{
+                auto& inWire = parts[0];
+                auto& shift = parts[2];
+                if(std::isdigit(inWire[0])) presetSignals[inWire] = (uint16_t)std::stoul(inWire);
+                gates.emplace_back(rshiftGate(&signals, std::move(out), std::move(inWire), std::move(shift) ));
+            }
+        }
+    }
+
+
+    auto getSignalAtWire(const std::string& s){
+        for(const auto& [wire, val] : presetSignals){
+            signals[wire] = val;
+        }
+
+        auto signalPassedThrough = [this](auto& gate){
+            const auto isReady = std::visit([](const auto& gate) -> bool {
+                return gate.isReady();
+            }, gate);
+
+            if(isReady){
+                std::visit([](const auto& gate){ gate.passSignal(); }, gate);
+                return true;
+            }
+            return false;
+        };
+
+        while(!signals.contains(s))
+        {
+            std::ranges::for_each(gates, signalPassedThrough);
+        }
+
+        return signals[s];
+    }
+
+    auto get(const std::string& s) noexcept {
+        return signals[s];
+    }
+
+    auto set(const std::string& s, const auto& val){
+        presetSignals[s] = val;
+    }
+
+    void reset(){
+        signals.clear();
+    }
+};
+
 
 int main(){
-    const auto input = readFile::vectorOfStrings("input.txt");
-
-    std::unordered_map<std::string, uint16_t> signals;
-    auto gates = parseInput(input, signals);
+    Circuit circuit{};
+    circuit.createGates(readFile::vectorOfStrings("input.txt"));
 
     //Task 1
-    propagateSignals(signals, gates);
-    std::cout << "Wire a provides signal " << signals["a"] << ".\n";
+    const auto signalAtWireA = circuit.getSignalAtWire("a");
+    std::cout << "Wire a provides signal " << signalAtWireA << ".\n";
 
     //Task 2
-    uint16_t newB = signals["a"];
-    signals.clear();
-    gates = parseInput(input, signals);
-    signals["44430"] = newB;
-
-    propagateSignals(signals, gates);
-    std::cout << "Wire a provides signal " << signals["a"] << " now.\n";
+    circuit.reset();
+    circuit.set("b", signalAtWireA);
+    const auto newSignalAtWireA = circuit.getSignalAtWire("a");
+    std::cout << "Wire a provides signal " << newSignalAtWireA << " now.\n";
 
 }
