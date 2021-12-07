41bbbc1175b762b3db8bf907070b8870cdef1a84
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-07 17:06:15 +0100

    Cleanup

--- 2015/Day08/tasks.cpp
+++ 2015/Day08/tasks.cpp
@@ -1,41 +1,38 @@
 
 #include "../../lib/readFile.hpp"
+#include "../../lib/utilities.hpp"
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <numeric>
-#include <functional>
 
 auto countEscapedCharacters = [](const auto& s){
-    size_t escapedCharacters = 0u;
+    auto escapedCharacters = 0u;
     auto it = std::ranges::find(s, '\\')+1;
     while( it < std::end(s) ){
-        *it == 'x' ? escapedCharacters+=3 : escapedCharacters++;
+        escapedCharacters += (*it == 'x' ? 3 : 1);
         if(*it == '\\') it++;
-        it = std::find(it, std::end(s), '\\')+1;
+        it = std::ranges::find(it, std::end(s), '\\')+1;
     }
-    return escapedCharacters + 2; //2 for the outside ""
+    return escapedCharacters + 2; //+2 for the surrounding ""
 };
 
 auto encodeCharacters = [](const auto& s){
     auto needsEscaping = [](const auto& c){
         return c == '\\' || c == '\"';
     };
-    return std::ranges::count_if(s, needsEscaping) + 2; //2 for the outside ""
+    return std::ranges::count_if(s, needsEscaping) + 2; //+2 for the outside ""
 };
 
 int main(){
     const auto strings = readFile::vectorOfStrings("input.txt");
 
     //Task 1
-    auto escapedCharacters = std::transform_reduce(std::begin(strings), std::end(strings), 0u,
-                                                   std::plus<unsigned int>(), countEscapedCharacters);
+    const auto escapedCharacters = Utilities::sum( strings, 0u, countEscapedCharacters );
     std::cout << "The answer for task 1 is " << escapedCharacters << '\n';
 
     //Task 2
-    auto encodedSize = std::transform_reduce(std::begin(strings), std::end(strings), 0u,
-                                                   std::plus<unsigned int>(), encodeCharacters);
+    const auto encodedSize = Utilities::sum( strings, 0u, encodeCharacters );
     std::cout << "The answer for task 2 is " << encodedSize << '\n';
 }
