81551b2b55e2211b7503a3fdf2c227583b355ad0
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-08 18:44:35 +0100

    Using matrix class

--- 2015/Day13/tasks.cpp
+++ 2015/Day13/tasks.cpp
@@ -1,6 +1,7 @@
 
 #include "../../lib/readFile.hpp"
 #include "../../lib/utilities.hpp"
+#include "../../lib/matrix.hpp"
 
 #include <iostream>
 #include <string>
@@ -10,37 +11,39 @@
 #include <numeric>
 #include <ranges>
 
+
 auto fillHappinessMatrix = [](const auto& input){
 
-    size_t N = (size_t)(1+std::sqrt(4*input.size()+1))/2;
-    std::vector<std::vector<int>> happinessMatrix(N, std::vector<int>(N));
+    const auto N = (size_t)(1+std::sqrt(4*input.size()+1))/2;
+    // std::vector<std::vector<int>> happinessMatrix(N, std::vector<int>(N));
+    Matrix::Matrix<int> happinessMatrix(N, N);
 
     for(size_t i : std::views::iota(0u,N)){
         for(size_t j=0; j<i; j++){
             auto split = Utilities::split(input[i*(N-1)+j]);
-            happinessMatrix[i][j] = std::stoi(split[3]);
-            if(split[2] == "lose") happinessMatrix[i][j]*=-1;
+            happinessMatrix(i,j) = std::stoi(split[3]);
+            if(split[2] == "lose") happinessMatrix(i,j)*=-1;
         }
         for(size_t j=i+1; j<N; j++){
             auto split = Utilities::split(input[i*(N-1)+j-1]);
-            happinessMatrix[i][j] = std::stoi(split[3]);
-            if(split[2] == "lose") happinessMatrix[i][j]*=-1;
+            happinessMatrix(i,j) = std::stoi(split[3]);
+            if(split[2] == "lose") happinessMatrix(i,j)*=-1;
         }
     }
     return happinessMatrix;
 };
 
 auto makeEveryoneHappy = [](const auto& happinessMatrix){
-    size_t N =happinessMatrix.size();
+    auto N =happinessMatrix.rows();
     std::vector<size_t> order(N-1);
     std::iota(std::begin(order), std::end(order), 0u);
     int maxHappiness = 0;
     do{
-        int happiness = happinessMatrix[order[N-2]][N-1] + happinessMatrix[N-1][order[N-2]];
+        int happiness = happinessMatrix(order[N-2], N-1) + happinessMatrix(N-1, order[N-2]);
         size_t lastPerson = N-1;
         for(size_t nextPerson : order){
-            happiness+=happinessMatrix[lastPerson][nextPerson];
-            happiness+=happinessMatrix[nextPerson][lastPerson];
+            happiness+=happinessMatrix(lastPerson,nextPerson);
+            happiness+=happinessMatrix(nextPerson,lastPerson);
             lastPerson=nextPerson;
         }
         if(happiness > maxHappiness) maxHappiness=happiness;
@@ -49,15 +52,15 @@ auto makeEveryoneHappy = [](const auto& happinessMatrix){
 };
 
 auto addMyself = [](auto& happinessMatrix){
-    size_t N = happinessMatrix.size();
-    for(auto& v : happinessMatrix) v.push_back(0);
-    happinessMatrix.push_back(std::vector<int>(N+1));
+    const auto N = happinessMatrix.rows();
+    happinessMatrix.resize(N+1, N+1);
+
+    // for(auto& v : happinessMatrix) v.push_back(0);
+    // happinessMatrix.push_back(std::vector<int>(N+1, 0));
 };
 
 int main(){
-    const auto input = readFile::vectorOfStrings("input.txt");
-
-    auto happinessMatrix = fillHappinessMatrix(input);
+    auto happinessMatrix = fillHappinessMatrix(readFile::vectorOfStrings("input.txt"));
 
     //Task 1
     auto happiness = makeEveryoneHappy(happinessMatrix);
