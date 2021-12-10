5e2d5070b97c9dd9269dca81c6bf2b3b953ed610
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2021-12-10 16:45:14 +0100

    Small cleanup

--- 2015/Day25/tasks.cpp
+++ 2015/Day25/tasks.cpp
@@ -10,12 +10,12 @@ int main(){
 
     const auto row = std::stoul(strings[15]);
     const auto col = std::stoul(strings[17]);
-    const auto n = col+(row+col-2)*(row+col-1)/2;
+    const auto n   = col+(row+col-2)*(row+col-1)/2;
 
-    unsigned long long int code = 20151125;
-    for(auto i=1u; i<n; i++){
-        code*=252533u;
-        code%=33554393u;
+    auto code = 20'151'125ul;
+    for(auto i=1ul; i<n; i++){
+        code*=252'533ul;
+        code%=33'554'393ul;
     }
 
     std::cout << "The machine is requesting the code " << code << ".\n";
