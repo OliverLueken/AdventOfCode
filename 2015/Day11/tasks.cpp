dd07881f307ae110e45bd9858ce32fb7da019981
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-04 19:42:09 +0100

    Fixing variable shadowing

--- 2015/Day11/tasks.cpp
+++ 2015/Day11/tasks.cpp
@@ -8,27 +8,27 @@
 #include <ranges>
 
 auto getNewPassword = [](auto& pw){
-    auto hasThreeLettersInIncreasingOrder = [](const auto& pw){
-        for(auto it = std::begin(pw)+2; it!=std::end(pw); it++){
+    auto hasThreeLettersInIncreasingOrder = [](const auto& password){
+        for(auto it = std::begin(password)+2; it!=std::end(password); it++){
             if( *(it-2)+2 == *(it-1)+1 && *(it-1)+1 == *it ) return true;
         }
         return false;
     };
-    auto hasTwoPairs = [](const auto& pw){
-        auto it = std::ranges::adjacent_find(pw);
-        it = std::adjacent_find(it+2, std::end(pw));
-        return it < std::end(pw);
+    auto hasTwoPairs = [](const auto& password){
+        auto it = std::ranges::adjacent_find(password);
+        it = std::adjacent_find(it+2, std::end(password));
+        return it < std::end(password);
     };
-    auto incrementPW = [](auto& pw){
+    auto incrementPW = [](auto& password){
         auto isNotZ = [](const auto& c){return c != 'z'; };
         auto isForbiddenChar = [](const auto& c){
             return c=='i' || c=='o' || c=='l';
         };
 
-        auto it = std::ranges::find_if( pw | std::views::reverse, isNotZ );
+        auto it = std::ranges::find_if( password | std::views::reverse, isNotZ );
         *it+=1;
         if(isForbiddenChar(*it)) *it+=1;
-        std::fill(std::rbegin(pw), it, 'a');
+        std::fill(std::rbegin(password), it, 'a');
     };
 
     do{
