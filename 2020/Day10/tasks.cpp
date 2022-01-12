4a4502b8033edcb1e16a90380e325c0383d71a57
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-12 14:41:48 +0100

    Refactoring

--- 2020/Day10/tasks.cpp
+++ 2020/Day10/tasks.cpp
@@ -9,43 +9,51 @@
 auto parseInput(auto&& input){
     input.push_back(0);
     std::ranges::sort(input);
-    return std::move(input);
+    input.push_back(input.back()+3);
+    std::vector<int> differences{};
+    for(auto i=1u; i<input.size(); i++){
+        differences.push_back( input[i]-input[i-1] );
+    }
+    return differences;
+}
+
+auto getProductOfDifferenceCounts(const auto& differences){
+    std::unordered_map<int, int> differenceCounts{};
+    for(const auto& difference : differences){
+        differenceCounts[difference]++;
+    }
+
+    return differenceCounts[1]*differenceCounts[3];
 }
 
 auto possibleArrangementsOfNConsecutiveAdapters(const auto n){
     return (1<<std::max(n-1,0)) - (1<<std::max(n-3,0)) + 1;
 }
 
-auto getResults(const auto& adapters){
+auto getTotalPossibleArrangements(const auto& differences){
     auto totalPossibleArrangements = 1ul;
-    std::unordered_map<int, int> differenceCounts{};
-    auto consecutiveAdapterCount = 0;
-    for(auto i=0u; i<adapters.size()-1; i++){
-        const auto difference = adapters[i+1]-adapters[i];
-        differenceCounts[difference]++;
+    auto consecutiveAdapterCount   = 0;
+    for(const auto& difference : differences){
         switch(difference){
         break; case 1:
             consecutiveAdapterCount++;
         break; case 3:
-            totalPossibleArrangements*=possibleArrangementsOfNConsecutiveAdapters(consecutiveAdapterCount);
-            consecutiveAdapterCount = 0;
+            totalPossibleArrangements *= possibleArrangementsOfNConsecutiveAdapters(consecutiveAdapterCount);
+            consecutiveAdapterCount    = 0;
         }
     }
-
-    differenceCounts[3]++;
-    const auto productOfDifferenceCounts=differenceCounts[1]*differenceCounts[3];
-    totalPossibleArrangements*=possibleArrangementsOfNConsecutiveAdapters(consecutiveAdapterCount);
-    return std::make_pair(productOfDifferenceCounts, totalPossibleArrangements);
+    return totalPossibleArrangements;
 }
 
 int main(){
-    const auto adapters = parseInput(readFile::vectorOfInts());
+    const auto differences = parseInput(readFile::vectorOfInts());
 
     //Task 1
-    const auto [productOfDifferenceCounts, totalPossibleArrangements] = getResults(adapters);
+    const auto productOfDifferenceCounts = getProductOfDifferenceCounts(differences);
     std::cout << "The product of the two difference counts is " << productOfDifferenceCounts << ".\n";
 
     //Task 2
+    const auto totalPossibleArrangements = getTotalPossibleArrangements(differences);
     std::cout << "There are a total of " << totalPossibleArrangements << " possible adapter arrangements.\n";
 
     VerifySolution::verifySolution(productOfDifferenceCounts, totalPossibleArrangements);
