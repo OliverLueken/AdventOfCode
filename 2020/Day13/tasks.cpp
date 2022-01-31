9415ff5e544fbf7d76082a984f4b7462d4a5d4b8
Oliver Lüken <oliver.lueken@pm.me>
2022-01-31 11:27:56 +0100

    Improving Day 13 solution

--- 2020/Day13/tasks.cpp
+++ 2020/Day13/tasks.cpp
@@ -1,97 +1,46 @@
+
+#include "../../lib/readFile.hpp"
+#include "../../lib/utilities.hpp"
+#include "../../lib/verifySolution.hpp"
+
+#include <algorithm>
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+#include <ranges>
 
 
-void earliestbus(std::vector<std::string>& input, int& result){
-  int t=stoi(input[0]);
-  std::vector<int> ids;
-  size_t a=0, b=0;
-  while(b!=std::string::npos){
-    b=input[1].find(",",a);
-    try{
-      ids.push_back(stoi(input[1].substr(a,b-a)));
+auto parseInput(const auto& input){
+    const auto startTime = std::stoi(input[0]);
+    const auto busIDs = Utilities::split(input[1], ',');
+    std::vector<std::pair<int,int>> idOffsetPair{};
+    for(auto i=0u; i<busIDs.size(); ++i){
+        if(busIDs[i][0]=='x') continue;
+        const auto id = std::stoi(busIDs[i]);
+        idOffsetPair.emplace_back(id, id-i);
     }
-    catch (...){}
-    a=b+1;
-  }
-  int min=t, minid;
-  for(auto id:ids){
-    int a=id-t%id;
-    if(a<min){
-      min=a;
-      minid=id;
-    }
-  }
-  result=minid*min;
+    return std::make_pair(startTime, idOffsetPair);
 }
 
-void besttime(std::vector<std::string>& input, long& result){
-
-  size_t a=0, b=0;
-  std::vector<int> ni, bi;
-  int i=0;
-  long N=1;
-  while(b!=std::string::npos){
-    b=input[1].find(",",a);
-    try{
-      ni.push_back(stoi(input[1].substr(a,b-a)));
-      bi.push_back(ni.back()-i);
-      N*=ni.back();
-      std::cout << ni.back() << ", " << bi.back() << std::endl;
-    }
-    catch (...){}
-    i++;
-    a=b+1;
-  }
-  std::cout << "N=" << N << std::endl;
-  //chinese remainder theorem
-  long x;
-  result=0;
-  for(int i=0; i<ni.size(); i++){
-    long Ni=N/ni[i];
-    int tempNi = Ni%ni[i];
-
-    int x=1;
-    for(; x<ni[i]; x++){
-      if(x*tempNi%ni[i]==1){
-        break;
-      }
-    }
-    std::cout << Ni << ", " << x << std::endl;
-    result+=x*Ni*bi[i];
-  }
-  result%=N;
+auto getEarliestBusTimesWaitingTime(const auto startTime, const auto& ids) {
+    const auto minIt = std::ranges::min_element( ids | std::views::elements<0>, std::less<>{},
+        [startTime](const auto& id){
+            return id-startTime%id;
+        }
+    );
+    return *minIt * (*minIt-startTime%*minIt);
 }
 
+int main() {
+    const auto [startTime, idOffsetPair] = parseInput(readFile::vectorOfStrings());
 
-std::vector<std::string> readfile(std::string file){
-  std::string line;
-  std::ifstream input(file);
-  std::vector<std::string> lines;
+    // Task 1
+    const auto earliestBusTimesWaitingtime = getEarliestBusTimesWaitingTime(startTime, idOffsetPair);
+    std::cout << "The first bus ID times the minutes to wait is " << earliestBusTimesWaitingtime << ".\n";
 
-  if(input.is_open()){
-  	while(getline(input,line)){
-        lines.push_back(line);
-    	}
-      input.close();
-    }
-  else{
-    std::cout << "Unable to open file\n";
-  }
-  return lines;
-}
+    // Task 2
+    const auto timeBusIDsAlign = Utilities::solveCRM(idOffsetPair);
+    std::cout << "The earliest timesstamp the bus depart with an offset of their IDs is " << timeBusIDsAlign << ".\n";
 
-int main(){
-  std::vector<std::string> input=readfile("input.txt");
-
-  int result1; long result2=1;
-  earliestbus(input, result1);
-  besttime(input, result2);
-
-  std::cout << result1 << "\n";
-  std::cout << result2 << "\n";
+    VerifySolution::verifySolution(earliestBusTimesWaitingtime, timeBusIDsAlign);
 }
