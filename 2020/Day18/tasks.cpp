28554b596b9c39d049207e90b4dd1590b2878d13
Oliver Lüken <oliver.lueken@pm.me>
2022-02-08 16:02:09 +0100

    Renaming & refactoring

--- 2020/Day18/tasks.cpp
+++ 2020/Day18/tasks.cpp
@@ -16,71 +16,70 @@ template<precedenceOrder>
 auto innerExpression(std::string&&);
 
 template<>
-auto innerExpression<leftToRight>(std::string&& ex) {
-    auto a = ex.find_first_of("+*");
+auto innerExpression<leftToRight>(std::string&& expression) {
+    auto a = expression.find_first_of("+*");
     while (a != std::string::npos) {
-        const auto b = ex.find_first_of("+*", a + 1);
-        auto x = stoul(ex);
-        auto y = stoul(ex.substr(a + 1));
-        if (ex[a] == '+') {
-            x = x + y;
+        const auto b = expression.find_first_of("+*", a + 1);
+        const auto leftValue  = stoul(expression);
+        const auto rightValue = stoul(expression.substr(a + 1));
+        if (expression[a] == '+') {
+            expression.replace(0, b, std::to_string(leftValue+rightValue));
         } else {
-            x = x * y;
+            expression.replace(0, b, std::to_string(leftValue*rightValue));
         }
-        ex.replace(0, b, std::to_string(x));
-        a = ex.find_first_of("+*");
+        a = expression.find_first_of("+*");
     }
-    return ex;
+    return expression;
 }
 
 template<>
-auto innerExpression<additionBeforeMultiplication>(std::string&& ex) {
-    auto b = ex.find("+");
+auto innerExpression<additionBeforeMultiplication>(std::string&& expression) {
+    auto b = expression.find("+");
     while (b != std::string::npos) {
-        auto a = ex.find_last_of("+*", b - 1);
+        auto a = expression.find_last_of("+*", b - 1);
         if (a == std::string::npos)
             a = 0;
         else
             a = a + 2;
-        const auto c = ex.find_first_of("+*", b + 1);
+        const auto c = expression.find_first_of("+*", b + 1);
 
-        const auto y = stoul(ex.substr(b + 1));
-        const auto x = stoul(ex.substr(a)) + y;
-        ex.replace(a, c - 1 - a, std::to_string(x));
+        const auto leftValue  = stoul(expression.substr(a));
+        const auto rightValue = stoul(expression.substr(b + 1));
+        expression.replace(a, c - 1 - a, std::to_string(leftValue+rightValue));
 
-        b = ex.find("+");
+        b = expression.find("+");
     }
-    return innerExpression<leftToRight>(std::move(ex));
+    return innerExpression<leftToRight>(std::move(expression));
 }
 
 template<precedenceOrder order>
-auto outerExpression = [regex = std::regex("([(][^()]+[)])")](auto& ex) {
+auto evaluateExpression = [regex = std::regex("([(][^()]+[)])")](auto& expression) {
     //regex matches every most inner bracketed expression including the brackets
     //9 * 3 * 4 + (8 + 3) + (7 + 9 + 9 * 8 + 3 + (8 + 5 + 2 + 9))
     //eg          ^^^^^^^<-this           and    ^^^^^^^^^^^^^^^<-this
     std::smatch match;
-    while (std::regex_search(ex, match, regex)) {
+    while (std::regex_search(expression, match, regex)) {
         const auto eval = innerExpression<order>(match.str().substr(1, match.length() - 2));
-        ex.replace(match.position(), match.length(), eval);
+        expression.replace(match.position(), match.length(), eval);
     }
-    return stoul(innerExpression<order>(std::move(ex)));
+    return stoul(innerExpression<order>(std::move(expression)));
 };
 
 template<precedenceOrder order>
-auto evalAll(auto input) {
-    return Utilities::sum(input, 0ul, outerExpression<order>);
+auto sumSolutions(auto input) {
+    return Utilities::sum(input, 0ul, evaluateExpression<order>);
 }
 
 int main(){
     const auto input = readFile::vectorOfStrings();
 
     //Task 1
-    const auto result1 = evalAll<leftToRight>(input);
-    std::cout << " " << result1 << " .\n";
+    const auto sumOfSolutionsWithLeftToRight = sumSolutions<leftToRight>(input);
+    std::cout << "The sum of the solutions evaluated left to right is " << sumOfSolutionsWithLeftToRight << ".\n";
 
     //Task 2
-    const auto result2 = evalAll<additionBeforeMultiplication>(input);
-    std::cout << " " << result2 << " .\n";
+    const auto sumOfSolutionsWithAdditionBeforeMultiplication = sumSolutions<additionBeforeMultiplication>(input);
+    std::cout << "The sum of the solutions evaluated with the new rules is " << sumOfSolutionsWithAdditionBeforeMultiplication << ".\n";
 
-    VerifySolution::verifySolution(result1, result2);
+    VerifySolution::verifySolution(sumOfSolutionsWithLeftToRight, sumOfSolutionsWithAdditionBeforeMultiplication);
 }
