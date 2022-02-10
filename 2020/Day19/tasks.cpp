c2cf95d68b3b3a3fc08dc782bcd0ebff7e43c54e
Oliver Lüken <oliver.lueken@pm.me>
2022-02-10 15:49:23 +0100

    Replacing old main with new main structure

--- 2020/Day19/tasks.cpp
+++ 2020/Day19/tasks.cpp
@@ -1,146 +1,140 @@
-#include <iostream>
+
+#include "../../lib/readFile.hpp"
+#include "../../lib/utilities.hpp"
+#include "../../lib/verifySolution.hpp"
+
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <cmath>
 #include <fstream>
+#include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <regex>
-#include <boost/algorithm/string.hpp>
 
 typedef std::vector<std::string> strvec;
 
-unsigned long result1=0, result2=0;
+unsigned long result1 = 0, result2 = 0;
 
-std::string convertRules(const strvec& rules, std::map<int, std::string>& donerules, std::string currrule){
-  // std::cout << currrule << std::endl;
+std::string convertRules(const strvec& rules,
+                         std::map<int, std::string>& donerules,
+                         std::string currrule) {
+    // std::cout << currrule << std::endl;
 
-  std::string rule;
-  size_t a = currrule.find("|");
-  if(a!=std::string::npos){
-    std::string str1, str2;
-    str1 = currrule.substr(0,a-1);
-    str2 = currrule.substr(a+2);
+    std::string rule;
+    size_t a = currrule.find("|");
+    if (a != std::string::npos) {
+        std::string str1, str2;
+        str1 = currrule.substr(0, a - 1);
+        str2 = currrule.substr(a + 2);
 
-      // std::cout << "\"" << str1 << "\"" << "|" << "\"" << str2 << "\"" << std::endl;
-    rule = "(" + convertRules(rules, donerules, str1) + "|" + convertRules(rules, donerules, str2) + ")";
-  }
-  else{
-    a= currrule.find(" ");
-    if(a!=std::string::npos){
+        // std::cout << "\"" << str1 << "\"" << "|" << "\"" << str2 << "\"" <<
+        // std::endl;
+        rule = "(" + convertRules(rules, donerules, str1) + "|" +
+               convertRules(rules, donerules, str2) + ")";
+    } else {
+        a = currrule.find(" ");
+        if (a != std::string::npos) {
+            std::vector<std::string> splitvec;
 
-        std::vector<std::string> splitvec;
+            boost::split(splitvec, currrule, boost::is_any_of(" "));
+            // for(auto str:splitvec){
+            //   std::cout << "\"" << str << "\"" << " ";
+            // }
+            // std::cout << std::endl;
+            for (auto str : splitvec) {
+                rule += convertRules(rules, donerules, str);
+            }
+        } else {
+            int n;
+            try {
+                n = stoi(currrule);
+            } catch (...) {
+                rule = "(" + currrule.substr(1, currrule.size() - 2) + ")";
+                // std::cout << rule << std::endl;
+                return rule;
+            }
 
-        boost::split(splitvec, currrule, boost::is_any_of(" "));
-        // for(auto str:splitvec){
-        //   std::cout << "\"" << str << "\"" << " ";
-        // }
-        // std::cout << std::endl;
-        for(auto str:splitvec){
-          rule += convertRules(rules, donerules, str);
+            auto it = donerules.find(n);
+            if (it != donerules.end()) return it->second;
+            rule = convertRules(rules, donerules, rules[n]);
+            // //part 2 (not a good solution, but works)
+            // if(n==8) rule+="+";
+            // if(n==11){
+            //   std::string rule42 = convertRules(rules, donerules, "42");
+            //   std::string rule31 = convertRules(rules, donerules, "31");
+            //   std::vector<std::string> t;
+            //   rule=rule42 + rule31;
+            //   t.push_back(rule);
+            //   for(int i=0; i<5; i++){
+            //      rule = rule42 + t.back() + rule31;
+            //      t.push_back(rule);
+            //   }
+            //   rule=t[0];
+            //   for(int i=1; i<t.size(); i++){
+            //     rule=rule + "|" +"(" + t[i] + ")";
+            //   }
+            //   rule = "("+rule+")";
+            //   std::cout << rule << std::endl << std::endl;
+            // }
+            donerules[n] = rule;
         }
     }
-    else{
-      int n;
-      try{
-        n=stoi(currrule);
-      }
-      catch(...){
-        rule = "("+currrule.substr(1,currrule.size()-2)+")";
-        // std::cout << rule << std::endl;
-        return rule;
-      }
+    // std::cout << rule << std::endl;
+    return rule;
+}
 
-      auto it = donerules.find(n);
-      if(it!=donerules.end()) return it->second;
-      rule = convertRules(rules, donerules, rules[n]);
-      //part 2 (not a good solution, but works)
-      // if(n==8) rule+="+";
-      // if(n==11){
-      //   std::string rule42 = convertRules(rules, donerules, "42");
-      //   std::string rule31 = convertRules(rules, donerules, "31");
-      //   std::vector<std::string> t;
-      //   rule=rule42 + rule31;
-      //   t.push_back(rule);
-      //   for(int i=0; i<5; i++){
-      //      rule = rule42 + t.back() + rule31;
-      //      t.push_back(rule);
-      //   }
-      //   rule=t[0];
-      //   for(int i=1; i<t.size(); i++){
-      //     rule=rule + "|" +"(" + t[i] + ")";
-      //   }
-      //   rule = "("+rule+")";
-      //   std::cout << rule << std::endl << std::endl;
-      // }
-      donerules[n]=rule;
+std::regex convertRulesToRegex(strvec& rules) {
+    std::map<int, std::string> donerules;
+    std::string rule = convertRules(rules, donerules, rules[0]);
+    std::cout << rule << std::endl;
+    return std::regex(rule);
+}
+
+bool isMessageValid(std::string& message, const std::regex& rule) {
+    return std::regex_match(message, rule);
+}
+
+auto countValidMessages(strvec& messages, const std::regex& rule) {
+
+    for (auto& message : messages) {
+        if (isMessageValid(message, rule)) result1++;
     }
-  }
-  // std::cout << rule << std::endl;
-  return rule;
-
+    return result1;
 }
 
-std::regex convertRulesToRegex(strvec& rules){
-  std::map<int, std::string> donerules;
-  std::string rule = convertRules(rules, donerules, rules[0]);
-  std::cout << rule << std::endl;
-  return std::regex(rule);
-}
+auto extractRuleFromMessages(strvec& messages) {
+    std::regex rule;
+    strvec rules;
+    for (auto it = messages.begin(); it != messages.end();) {
+        // std::cout << *it << std::endl;
+        if (it->empty()) {
+            messages.erase(it);
+            break;
+        }
 
-bool isMessageValid(std::string& message, std::regex& rule){
-  return std::regex_match(message, rule);
-}
+        auto pos = stoul(*it);
+        std::string rulestr = it->substr(it->find(":") + 2);
+        if (pos >= rules.size()) rules.resize(pos + 1);
+        rules[pos] = rulestr;
 
-void countValidMessages(strvec& messages, std::regex& rule){
-  for(auto& message:messages){
-    if(isMessageValid(message, rule)) result1++;
-  }
-}
-
-void extractRuleFromMessages(strvec& messages, std::regex& rule){
-  strvec rules;
-  for(auto it=messages.begin(); it!=messages.end(); ){
-    // std::cout << *it << std::endl;
-    if(it->empty()){
-      messages.erase(it);
-      break;
+        it = messages.erase(it);
     }
 
-    int pos = stoi(*it);
-    std::string rulestr = it->substr(it->find(":")+2);
-    if(pos >= rules.size()) rules.resize(pos+1);
-    rules[pos]=rulestr;
-
-    it=messages.erase(it);
-  }
-
-  rule = convertRulesToRegex(rules);
-}
-
-strvec readfile(std::string file){
-  std::string line;
-  std::ifstream input(file);
-  strvec lines;
-
-  if(input.is_open()){
-  	while(getline(input,line)){
-        lines.push_back(line);
-    	}
-      input.close();
-    }
-  else{
-    std::cout << "Unable to open file\n";
-  }
-  return lines;
+    return convertRulesToRegex(rules);
 }
 
 int main(){
-  strvec input=readfile("input.txt");
+    auto input = readFile::vectorOfStrings("input.txt", '\n', true);
+    const auto rule = extractRuleFromMessages(input);
 
-  std::regex rule;
-  extractRuleFromMessages(input, rule);
-  countValidMessages(input, rule);
+    //Task 1
+    const auto sumOfSolutionsWithLeftToRight = countValidMessages(input, rule);
+    std::cout << "The sum of the solutions evaluated left to right is " << sumOfSolutionsWithLeftToRight << ".\n";
 
-  std::cout << result1 << "\n";
-  std::cout << result2 << "\n";
+    //Task 2
+    const auto sumOfSolutionsWithAdditionBeforeMultiplication = countValidMessages(input, rule);
+    std::cout << "The sum of the solutions evaluated with the new rules is " << sumOfSolutionsWithAdditionBeforeMultiplication << ".\n";
+
+    VerifySolution::verifySolution(sumOfSolutionsWithLeftToRight, sumOfSolutionsWithAdditionBeforeMultiplication);
 }
