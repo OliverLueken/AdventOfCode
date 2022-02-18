b7fc385e431dea2b0c3b451ab99409acbaff19ad
Oliver Lüken <oliver.lueken@pm.me>
2022-02-18 14:59:29 +0100

    Adding const

--- 2020/Day12/tasks.cpp
+++ 2020/Day12/tasks.cpp
@@ -54,7 +54,7 @@ auto moveShip2(const auto& navigationInstructions){
     std::complex<double> waypointPos{10,1};
 
     for(const auto& [c, val_] : navigationInstructions){
-        auto val = static_cast<double>(val_);
+        const auto val = static_cast<double>(val_);
         switch(c){
             break; case 'N':
                 waypointPos+=std::complex<double>{0,val};
