3429a531ba14d692d4f052113fd82d4c90a7f5fd
Oliver Lüken <oliver.lueken@pm.me>
2022-03-05 18:58:23 +0100

    Adding Game2 derived class, virtual variants Game member functions etc

--- 2020/Day22/tasks.cpp
+++ 2020/Day22/tasks.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <queue>
 #include <regex>
-#include <set>
+#include <unordered_set>
 #include <string>
 #include <vector>
 
@@ -58,14 +58,18 @@ auto updateDeck(auto& game, const Winner winner, int a, int b){
     return Winner::NoWinner;
 }
 
-template<typename T>
 struct Game{
     Deck deck1{};
     Deck deck2{};
-    const T& roundWinner;
 
-    Game(const Deck& deck1_, const Deck& deck2_, const T& roundWinner_)
-        : deck1(deck1_), deck2(deck2_), roundWinner(roundWinner_){}
+    Game(const Deck& deck1_, const Deck& deck2_)
+        : deck1(deck1_), deck2(deck2_){}
+
+    Game(const Game&) = default;
+    Game& operator=(const Game&) = default;
+    Game(Game&&) = default;
+    Game& operator=(Game&&) = default;
+    virtual ~Game() = default;
 
     auto dealCards(){
         return std::make_pair(
@@ -78,11 +82,11 @@ struct Game{
         return deck1.gameOver() || deck2.gameOver();
     }
 
-    auto getWinner() const {
+    virtual Winner getWinner() const {
         return deck2.gameOver() ? Winner::Player1 : Winner::Player2;
     }
 
-    auto play(){
+    virtual Winner play(){
         while (!gameOver()){
             const auto [a, b] = dealCards();
 
@@ -95,14 +99,15 @@ struct Game{
     auto score(){
         return deck1.score()+deck2.score();
     }
+
+    virtual Winner roundWinner(const unsigned int a, const unsigned int b) const {
+        return a > b ? Winner::Player1 : Winner::Player2;
+    }
 };
 
-template<typename T, typename S>
-bool operator<(const Game<T>& game1, const Game<S>& game2) noexcept{
+bool operator<(const Game& game1, const Game& game2) noexcept{
     return game1.deck1 < game2.deck1 || game1.deck2 < game2.deck2;
 }
-
-auto game1RoundWinner = [](const unsigned int a, const unsigned int b){return a > b ? Winner::Player1 : Winner::Player2;};
 
 auto dealDeck(const strvec& input){
     auto make_deck = [](auto begin, auto end){
@@ -115,18 +120,10 @@ auto dealDeck(const strvec& input){
     const auto nextDeckIt = std::ranges::find(input, "Player 2:");
     return Game{
         make_deck(std::begin(input)+1, nextDeckIt),
-        make_deck(nextDeckIt+1       , std::end(input)),
-        game1RoundWinner
+        make_deck(nextDeckIt+1       , std::end(input))
     };
 }
 
-bool deckAlreadyExisted(auto& game, auto& existingDecks){
-    // std::pair<Deck, Deck> p = {deck1, deck2};
-
-    auto it = existingDecks.insert(game);
-
-    return !it.second;
-}
 
 auto firstNCards(const Deck& d, const auto n){
     Deck newDeck{};
@@ -134,16 +131,52 @@ auto firstNCards(const Deck& d, const auto n){
     return newDeck;
 }
 
-Winner playGame2(auto game, unsigned long& result2, int depth = 0);
+struct Game2 : public Game{
+    std::unordered_set<size_t> existingDecks{};
 
-auto game2RoundWinner = [](const auto& game, unsigned long& result2, int depth, const unsigned int a, const unsigned int b){
-    // Do recursive call?
+    Game2(Game game_) : Game(game_){}
+    Game2(Deck deck1_, Deck deck2_) : Game{deck1_, deck2_} {}
+    Winner roundWinner(const unsigned int a, const unsigned int b) const;
+    Winner play();
+};
+
+template<>
+struct std::hash<Deck>{
+    std::size_t operator()(const Deck& deck) const noexcept{
+        size_t h = 0;
+        auto offset = 0;
+        for(const auto& i : deck){
+            h+=i<<offset;
+            offset=(offset+1)%32;
+        }
+        return h;
+    }
+};
+
+template<>
+struct std::hash<Game2>{
+    std::size_t operator()(const Game2& z) const noexcept{
+        const std::size_t h1 = std::hash<Deck>{}(z.deck1);
+        const std::size_t h2 = std::hash<Deck>{}(z.deck2);
+        return h1^(h2<<1);
+    }
+};
+
+bool deckAlreadyExisted(auto& game, auto& existingDecks){
+    // std::pair<Deck, Deck> p = {deck1, deck2};
+
+    auto it = existingDecks.insert(std::hash<Game2>{}(game));
+
+    return !it.second;
+}
+
+Winner Game2::roundWinner(const unsigned int a, const unsigned int b) const {
     auto roundWonBy = Winner::NoWinner;
-    if(game.deck1.size() >= a && game.deck2.size() >= b){
-        Deck deck1copy = firstNCards(game.deck1, a);
-        Deck deck2copy = firstNCards(game.deck2, b);
-        auto nextGame = Game{deck1copy, deck2copy, game.roundWinner};
-        roundWonBy = playGame2(nextGame, result2, depth + 1);
+    if(deck1.size() >= a && deck2.size() >= b){
+        Deck deck1copy = firstNCards(deck1, a);
+        Deck deck2copy = firstNCards(deck2, b);
+        auto nextGame = Game2{deck1copy, deck2copy};
+        roundWonBy = nextGame.play();
     }
     else{
         if (a > b)
@@ -152,29 +185,28 @@ auto game2RoundWinner = [](const auto& game, unsigned long& result2, int depth, 
             roundWonBy = Winner::Player2;
     }
     return roundWonBy;
-};
+}
 
-Winner playGame2(auto game, unsigned long& result2, int depth){
-    std::set<decltype(game)> existingDecks;
+Winner Game2::play(){
     auto gameWonBy = Winner::NoWinner;
 
     while(gameWonBy == Winner::NoWinner){
-
-        // Draw
-        const auto [a, b] = game.dealCards();
-
-
-        // auto roundWonBy = Winner::NoWinner;
-        const auto roundWonBy = game2RoundWinner(game, result2, depth, a, b);
-
-        gameWonBy = updateDeck(game, roundWonBy, a, b);
-
-        if(deckAlreadyExisted(game, existingDecks)){
+        if(deckAlreadyExisted(*this, existingDecks)){
             gameWonBy = Winner::Player1;
             break;
         }
-    }
+        const auto [a, b] = dealCards();
+        const auto roundWonBy = roundWinner(a, b);
+        gameWonBy = updateDeck(*this, roundWonBy, a, b);
 
+    }
+    return gameWonBy;
+}
+
+
+Winner playGame2(auto game_, unsigned long& result2, int depth = 0){
+    Game2 game{game_};
+    const auto gameWonBy = game.play();
     if(depth == 0){
         result2 = game.score();
     }
