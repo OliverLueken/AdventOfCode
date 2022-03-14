efea0667e6e808cc45c534d52d34335bc57f5352
Oliver Lüken <oliver.lueken@pm.me>
2022-03-14 14:31:23 +0100

    Formatting

--- 2020/Day25/tasks.cpp
+++ 2020/Day25/tasks.cpp
@@ -1,81 +1,79 @@
-#include <iostream>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <cmath>
 #include <fstream>
+#include <iostream>
+#include <queue>
+#include <regex>
+#include <set>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <regex>
-#include <boost/algorithm/string.hpp>
-#include <queue>
-#include <set>
 
 typedef std::set<std::string> strset;
 typedef std::vector<strset> strsetvec;
 typedef std::vector<std::string> strvec;
-typedef std::pair<int,int> point;
+typedef std::pair<int, int> point;
 
-unsigned long result1=0, result2=0;
+unsigned long result1 = 0, result2 = 0;
 
 void calcEncriptionKey(long k1, long k2, int l1, int l2){
-std::cout << k1 << ": " << l1 << std::endl
-          << k2 << ": " << l2 << std::endl;
-  // if(l2 < l1){
-  //   l1=l2;
-  // }else{
-  //   k1=k2;
-  // }
-  long s =k1;
-  long n=1;
-  for(int i=0; i<l2; i++){
-    n*=k1;
-    n%=20201227;
-  }
-  result1=n;
+    std::cout << k1 << ": " << l1 << std::endl << k2 << ": " << l2 << std::endl;
+    // if(l2 < l1){
+    //   l1=l2;
+    // }else{
+    //   k1=k2;
+    // }
+    long s = k1;
+    long n = 1;
+    for (int i = 0; i < l2; i++){
+        n *= k1;
+        n %= 20201227;
+    }
+    result1 = n;
 }
 
 int getLoopSize(long key){
-  long n = 1;
-  int loopsize = 0;
-  while(n!=key){
-    n*=7;
-    n%=20201227;
-    loopsize++;
-  }
-  return loopsize;
+    long n = 1;
+    int loopsize = 0;
+    while(n != key){
+        n *= 7;
+        n %= 20201227;
+        loopsize++;
+    }
+    return loopsize;
 }
 
 void doStuff(strvec& input){
-  long key1 = stol(input[0]),
-       key2 = stol(input[1]);
+    long key1 = stol(input[0]), key2 = stol(input[1]);
 
-   int l1= getLoopSize(key1);
-   int l2= getLoopSize(key2);
+    int l1 = getLoopSize(key1);
+    int l2 = getLoopSize(key2);
 
-   calcEncriptionKey(key1, key2, l1, l2);
+    calcEncriptionKey(key1, key2, l1, l2);
 }
 
 strvec readfile(std::string file){
-  std::string line;
-  std::ifstream input(file);
-  strvec lines;
+    std::string line;
+    std::ifstream input(file);
+    strvec lines;
 
-  if(input.is_open()){
-  	while(getline(input,line)){
-        lines.push_back(line);
-    	}
-      input.close();
+    if(input.is_open()){
+        while(getline(input, line)){
+            lines.push_back(line);
+        }
+        input.close();
     }
-  else{
-    std::cout << "Unable to open file\n";
-  }
-  return lines;
+    else{
+        std::cout << "Unable to open file\n";
+    }
+    return lines;
 }
 
 int main(){
-  strvec input=readfile("input.txt");
+    strvec input = readfile("input.txt");
 
-  doStuff(input);
+    doStuff(input);
 
-  std::cout << result1 << "\n";
-  std::cout << result2 << "\n";
+    std::cout << result1 << "\n";
+    std::cout << result2 << "\n";
 }
