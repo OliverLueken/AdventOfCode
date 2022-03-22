72d33f8ad0df6023ce16b199b4f5f177b3663f86
Oliver Lüken <oliver.lueken@pm.me>
2022-03-22 17:28:26 +0100

    Changing member names

--- lib/position.hpp
+++ lib/position.hpp
@@ -10,46 +10,46 @@ namespace Utilities{
     template<typename T>
     class Position{
     public:
-        T first{};
-        T second{};
+
+        T x{};
+        T y{};
+
 
         constexpr Position() = default;
 
-        constexpr Position(const T& _first, const T& _second)
-        : first{_first}, second{_second}{}
+        constexpr Position(const T& _x, const T& _y)
+        : x{_x}, y{_y}{}
 
-        constexpr Position(T&& _first, T&& _second)
-        : first{std::move(_first)}, second{std::move(_second)}{}
+        constexpr Position(T&& _x, T&& _y)
+        : x{std::move(_x)}, y{std::move(_y)}{}
 
         constexpr Position(const std::pair<T, T>& pair)
-        : first{pair.first}, second{pair.second}{}
+        : x{pair.first}, y{pair.second}{}
 
         constexpr auto
         operator==(const Position<T>& rhs) const {
-            return first == rhs.first && second == rhs.second;
+            return x == rhs.x && y == rhs.y;
         }
 
         constexpr auto
         operator<=>(const Position<T>& rhs) const {
-            if (first == rhs.first){
-                return second - rhs.second;
+            if (x == rhs.x){
+                return y - rhs.y;
             }
-            return first - rhs.first;
+            return x - rhs.x;
         }
     };
-    // template<class T = size_t>
-    // using Position = std::pair<T, T>;
 
     template<typename T, typename S>
     constexpr auto
-    make_position(const T& first, const S& second){
-        return Position<typename std::common_type<T, S>::type>{first, second};
+    make_position(const T& x, const S& y){
+        return Position<typename std::common_type<T, S>::type>{x, y};
     }
 
     template<typename T, typename S>
     constexpr auto
-    make_position(T&& first, S&& second){
-        return Position<typename std::common_type<T, S>::type>{std::move(first), std::move(second)};
+    make_position(T&& x, S&& y){
+        return Position<typename std::common_type<T, S>::type>{std::move(x), std::move(y)};
     }
 }
 
@@ -57,8 +57,8 @@ template<class T>
 struct std::hash<Utilities::Position<T>>{
         constexpr size_t
         operator()(const Utilities::Position<T>& position) const noexcept{
-        const std::size_t h1 = std::hash<T>{}(position.first);
-        const std::size_t h2 = std::hash<T>{}(position.second);
+        const std::size_t h1 = std::hash<T>{}(position.x);
+        const std::size_t h2 = std::hash<T>{}(position.y);
         return h1^(h2<<1);
     }
 };
@@ -69,27 +69,27 @@ template<typename T, typename S>
 requires Addable<T, S>
 constexpr Utilities::Position<T>
 operator+(const Utilities::Position<T>& lhs, const Utilities::Position<S>& rhs){
-    return Utilities::Position<T>{lhs.first+rhs.first, lhs.second+rhs.second};
+    return Utilities::Position<T>{lhs.x+rhs.x, lhs.y+rhs.y};
 }
 
 template<typename T, typename S>
 requires Subtractable<T, S>
 constexpr Utilities::Position<T>
  operator-(const Utilities::Position<T>& lhs, const Utilities::Position<S>& rhs){
-    return Utilities::Position<T>{lhs.first-rhs.first, lhs.second-rhs.second};
+    return Utilities::Position<T>{lhs.x-rhs.x, lhs.y-rhs.y};
 }
 
 template<typename T>
 constexpr Utilities::Position<T>
 operator/(const Utilities::Position<T>& numerator, const T& divisor){
-    return Utilities::Position<T>{numerator.first/divisor, numerator.second/divisor};
+    return Utilities::Position<T>{numerator.x/divisor, numerator.y/divisor};
 }
 
 template<typename T>
 requires Addable<T, T>
 constexpr T
 abs(const Utilities::Position<T>& position){
-    return std::abs(position.first)+std::abs(position.second);
+    return std::abs(position.x)+std::abs(position.y);
 }
 
 
