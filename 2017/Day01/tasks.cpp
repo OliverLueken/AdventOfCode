423626edc51f2c7baba59ce009a06cd01ff38c4f
Oliver Lüken <oliver.lueken@pm.me>
2022-04-07 15:06:42 +0200

    Adding Day1 task 1 solution

---
+++ 2017/Day01/tasks.cpp
@@ -0,0 +1,58 @@
+
+#include "../../lib/readFile.hpp"
+#include "../../lib/verifySolution.hpp"
+#include "../../lib/utilities.hpp"
+#include "../../lib/matrix.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <memory>
+#include <ranges>
+#include <cmath>
+#include <deque>
+
+using Position = Utilities::Position<int>;
+
+auto parseInput = [](const auto& input){
+    std::vector<int> parsed;
+    std::ranges::transform(input, std::back_inserter(parsed), [](const auto c){return c-'0';});
+    parsed.push_back(parsed.front());
+    return parsed;
+};
+
+auto getResult = [](const auto& parsedInput){
+    auto sum = 0;
+    for(auto it = parsedInput.begin(); it!=parsedInput.end()-1; ++it){
+        if(*it == *(it+1)){
+            sum+=*it;
+        }
+    }
+    return sum;
+};
+
+auto getResult2 = [](const auto& parsedInput){
+
+    return 0;
+};
+
+int main(){
+    const auto parsedInput = parseInput(readFile::string());
+
+    //Task 1
+    const auto result = getResult(parsedInput);
+    std::cout << "Task 1: " << result << ".\n";
+
+    // //Task 2
+    // const auto result2 = getResult2(parsedInput);
+    // std::cout << "Task 2: " << result2 << ".\n";
+
+    // VerifySolution::verifySolution(result, result2);
+}
