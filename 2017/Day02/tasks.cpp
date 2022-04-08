3e743e51c4a03e350d107f75902ce9bfb832cacf
Oliver Lüken <oliver.lueken@pm.me>
2022-04-08 11:07:40 +0200

    Adding solution for task 2

--- 2017/Day02/tasks.cpp
+++ 2017/Day02/tasks.cpp
@@ -25,7 +25,21 @@ auto getResult = [](const auto& parsedInput){
 
 auto getResult2 = [](const auto& parsedInput){
 
-    return 0;
+    auto getDivisiblePair = [](const auto& row){
+        for(auto i=0u; i<row.size(); ++i){
+            for(auto j=i+1; j<row.size(); ++j){
+                if(row[j]%row[i]==0){
+                    return std::make_pair(row[j], row[i]);
+                }
+            }
+        }
+        return std::make_pair(1,1);
+    };
+
+    return Utilities::sum(parsedInput, 0, [getDivisiblePair](const auto& row){
+        const auto [numerator, denominator] = getDivisiblePair(row);
+        return numerator/denominator;
+    });
 };
 
 int main(){
@@ -35,9 +49,9 @@ int main(){
     const auto result = getResult(parsedInput);
     std::cout << "Task 1: " << result << ".\n";
 
-    // //Task 2
-    // const auto result2 = getResult2(parsedInput);
-    // std::cout << "Task 2: " << result2 << ".\n";
+    //Task 2
+    const auto result2 = getResult2(parsedInput);
+    std::cout << "Task 2: " << result2 << ".\n";
 
-    // VerifySolution::verifySolution(result, result2);
+    VerifySolution::verifySolution(result, result2);
 }
