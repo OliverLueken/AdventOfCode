cd5796db40835613ecc2783be3778a2c396eb1c3
Oliver Lüken <oliver.lueken@pm.me>
2022-05-20 13:05:33 +0200

    Adding tasks.cpp

---
+++ 2017/Day11/tasks.cpp
@@ -0,0 +1,44 @@
+
+#include "../../lib/readFile.hpp"
+#include "../../lib/verifySolution.hpp"
+#include "../../lib/utilities.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+
+auto parseInput = [](const auto& input){
+    std::vector<> parsed;
+    for(const auto& row : input){
+
+    }
+    return parsed;
+};
+
+auto getResult = [](const auto& parsedInput){
+
+    return 0;
+};
+
+auto getResult2 = [](const auto& parsedInput){
+
+    return 0;
+};
+
+int main(){
+    const auto parsedInput = parseInput(readFile::vectorOfStrings());
+
+    //Task 1
+    const auto result = getResult(parsedInput);
+    std::cout << "Task 1: " << result << ".\n";
+
+    // //Task 2
+    // const auto result2 = getResult2(parsedInput);
+    // std::cout << "Task 2: " << result2 << ".\n";
+
+    // VerifySolution::verifySolution(result, result2);
+}
