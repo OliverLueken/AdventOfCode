07b963b64f735d4b97e5ced961ffadc14a8bf7f9
Oliver Lüken <oliver.lueken@pm.me>
2022-05-31 12:27:47 +0200

    Templating computer factory

--- 2017/Day05/tasks.cpp
+++ 2017/Day05/tasks.cpp
@@ -22,72 +22,60 @@ struct ExecutionLogger : public Computer::Logger {
     }
 };
 
+template<size_t mode>
+auto makeJumpInstruction(const int offset, Computer_* comp);
 
+
+template<>
+auto makeJumpInstruction<0>(const int offset, Computer_* comp){
+    auto jump = [_offset = offset, comp] () mutable {
+        comp->advanceCurrentPosition(_offset);
+        ++_offset;
+    };
+    return jump;
+}
+
+template<>
+auto makeJumpInstruction<1>(const int offset, Computer_* comp){
+    auto jump = [_offset = offset, comp] () mutable {
+        comp->advanceCurrentPosition(_offset);
+        if(_offset>=3) --_offset;
+        else           ++_offset;
+    };
+    return jump;
+}
+
+template<size_t mode>
 struct ComputerFactory{
-    static void addJump(const auto offset, Computer_* comp){
-        auto jump = [_offset = offset, comp] () mutable {
-            comp->advanceCurrentPosition(_offset);
-            ++_offset;
-        };
-        comp->add(std::move(jump));
-    }
-
     static Computer_ make(const auto& input){
         Computer_ comp{};
 
         for(const auto& offset : input){
-            addJump(offset, &comp);
+            comp.add(makeJumpInstruction<mode>(offset, &comp));
         }
         return comp;
     }
 };
 
-struct ComputerFactory2{
-
-    static Computer_ make(const auto& input){
-        Computer_ comp{};
-
-        for(const auto& offset : input){
-            addOddJump(offset, &comp);
-        }
-        return comp;
-    }
-
-    static void addOddJump(const auto offset, Computer_* comp){
-        auto jump = [_offset = offset, comp] () mutable {
-            comp->advanceCurrentPosition(_offset);
-            if(_offset>=3) --_offset;
-            else           ++_offset;
-        };
-        comp->add(std::move(jump));
-    }
-};
-
+template<size_t mode>
 auto getStepCount = [](const auto& input){
-    auto computer = ComputerFactory::make(input);
-    
-    auto logger = ExecutionLogger{&computer};
-    computer.execute();
-    return logger.executionCount;
-};
-
-auto getStepCountWithOddJumps = [](const auto& input){
-    auto computer = ComputerFactory2::make(input);
+    auto computer = ComputerFactory<mode>::make(input);
 
     auto logger = ExecutionLogger{&computer};
     computer.execute();
     return logger.executionCount;
 };
+
 
 int main(){
     const auto input = readFile::vectorOfInts();
 
     //Task 1
-    const auto firstStepCount = getStepCount(input);
+    const auto firstStepCount = getStepCount<0>(input);
     std::cout << "It takes " << firstStepCount << " steps to reach the exit.\n";
 
     //Task 2
-    const auto secondStepCount = getStepCountWithOddJumps(input);
+    const auto secondStepCount = getStepCount<1>(input);
     std::cout << "With the odd jumpts it takes " << secondStepCount << " steps to reach the exit.\n";
 
     VerifySolution::verifySolution(firstStepCount, secondStepCount);
