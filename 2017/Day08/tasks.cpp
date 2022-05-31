08d635c605ffbd54ad349b38968fb3e9944d8cc7
Oliver Lüken <oliver.lueken@pm.me>
2022-05-31 11:08:35 +0200

    Using computer.hpp

--- 2017/Day08/tasks.cpp
+++ 2017/Day08/tasks.cpp
@@ -1,6 +1,7 @@
 
 #include "../../lib/readFile.hpp"
 #include "../../lib/verifySolution.hpp"
+#include "../../lib/computer.hpp"
 
 #include <iostream>
 #include <vector>
@@ -10,132 +11,14 @@
 #include <functional>
 #include <algorithm>
 
-struct Logger{
-    virtual void update() = 0;
-};
+using Register = std::unordered_map<std::string, int>;
+using Computer_ = Computer::Computer<Register>;
 
-struct Subject{
-    std::vector<Logger*> logger{};
-    void addLogger(Logger* log){
-        logger.push_back(log);
-    }
-    void removeLogger(const Logger* log){
-        std::erase(logger, log);
-    }
-    void notifyLogger() const {
-        std::ranges::for_each(logger, [](Logger* log){log->update();});
-    }
-};
-
-class Computer : public Subject {
-
-    struct Instruction{
-        Instruction() = default;
-        Instruction(const Instruction&) = default;
-        Instruction& operator=(const Instruction&) = default;
-        Instruction(Instruction&&) = default;
-        Instruction& operator=(Instruction&&) = default;
-        virtual ~Instruction() = default;
-
-        virtual void execute() = 0;
-    };
-
-    template<typename Lambda>
-    struct Wrapper : public Instruction, public Lambda{
-        Wrapper(Lambda&& lambda): Lambda{std::forward<Lambda>(lambda)} {}
-        void execute() override { Lambda::operator()(); }
-    };
-
-    using Register = std::unordered_map<std::string, int>;
-    using Instructions = std::vector<std::unique_ptr<Instruction>>;
-
-    int currentInstructionPosition{0};
-    Instructions instructions{};
-    Register reg{};
-
-    template<typename Lambda>
-    auto add(Lambda&& l){
-        instructions.emplace_back(std::make_unique<Wrapper<Lambda>>(std::forward<Lambda>(l)));
-    }
-
-    auto getComparison(const auto comparator) -> std::function<bool(int, int)> const {
-        if(comparator == "<")  return std::less<>{};
-        if(comparator == "<=") return std::less_equal<>{};
-        if(comparator == ">")  return std::greater<>{};
-        if(comparator == ">=") return std::greater_equal<>{};
-        if(comparator == "==") return std::equal_to<>{};
-        return std::not_equal_to<>{};
-    }
-
-    bool currentInstructionPositionIsValid() const {
-        return 0<=currentInstructionPosition && std::less{}(currentInstructionPosition, instructions.size());
-    }
-
-public:
-    auto addJump(const auto offset){
-        auto jump = [_offset = offset, this] () mutable {
-            this->currentInstructionPosition+=_offset;
-            // ++_offset;
-        };
-        return add(std::move(jump));
-    }
-    //
-    // void addOddJump(const auto offset){
-    //     auto jump = [_offset = offset, this] () mutable {
-    //         this->currentInstructionPosition+=_offset;
-    //         if(_offset>=3) --_offset;
-    //         else           ++_offset;
-    //     };
-    //     instructions.emplace_back(std::make_unique<Wrapper<decltype(jump)>>(std::move(jump)));
-    // }
-
-    auto addIf(const auto comparator, const auto _regAddress, const auto _val){
-        auto _conditional = getComparison(comparator);
-        auto ifLambda = [regAddress = _regAddress, conditional = _conditional, val = _val, this](){
-            if(conditional( this->reg[regAddress], val )){
-                ++this->currentInstructionPosition;
-            }
-            else{
-                this->currentInstructionPosition+=2;
-            }
-        };
-        return add(std::move(ifLambda));
-    }
-
-    auto addRegisterModification(const auto regAddress, const auto mode, const auto amount){
-        if(mode == "inc") return addRegisterIncrease(regAddress, amount);
-        if(mode == "dec") return addRegisterDecrease(regAddress, amount);
-    }
-
-    auto addRegisterIncrease(const auto _regAddress, const auto _amount){
-        auto increase = [regAddress = _regAddress, amount = _amount, this] () mutable {
-            this->reg[regAddress]+=amount;
-            ++this->currentInstructionPosition;
-        };
-        return add(std::move(increase));
-    }
-
-    auto addRegisterDecrease(const auto _regAddress, const auto _amount){
-        return addRegisterIncrease(_regAddress, -1*_amount);
-    }
-
-    auto execute(){
-        for(currentInstructionPosition = 0; currentInstructionPositionIsValid();){
-            instructions[currentInstructionPosition]->execute();
-            notifyLogger();
-        }
-    }
-
-    auto getRegisterView() const {
-        return std::views::all(reg);
-    }
-};
-
-struct RegisterValueLogger : public Logger {
-    Computer* computer{nullptr};
+struct RegisterValueLogger : public Computer::Logger {
+    Computer_* computer{nullptr};
     std::vector<int> maxRegisterValueAfterEachExecution{};
 
-    RegisterValueLogger(Computer* comp) : computer{comp}{
+    RegisterValueLogger(Computer_* comp) : computer{comp}{
         computer->addLogger(this);
     }
 
@@ -146,15 +29,59 @@ struct RegisterValueLogger : public Logger {
     }
 };
 
-auto parseInput(const auto& input){
-    auto comp = Computer{};
-    for(const auto& instr : input){
-        auto split = Utilities::split(instr);
-        comp.addIf(split[5], split[4], std::stoi(split[6]));
-        comp.addRegisterModification(split[0], split[1], std::stoi(split[2]));
+
+
+struct ComputerFactory{
+    static auto getComparison(const auto comparator) -> std::function<bool(int, int)> const {
+        if(comparator == "<")  return std::less<>{};
+        if(comparator == "<=") return std::less_equal<>{};
+        if(comparator == ">")  return std::greater<>{};
+        if(comparator == ">=") return std::greater_equal<>{};
+        if(comparator == "==") return std::equal_to<>{};
+        return std::not_equal_to<>{};
     }
-    return comp;
-}
+
+    static auto addIf(const auto comparator, const auto _regAddress, const auto _val, Computer_* comp){
+        auto _conditional = getComparison(comparator);
+        auto ifLambda = [regAddress = _regAddress, conditional = _conditional, val = _val, comp](){
+            if(conditional( comp->reg->operator[](regAddress), val )){
+                ++comp->currentInstructionPosition;
+            }
+            else{
+                comp->currentInstructionPosition+=2;
+            }
+        };
+        return comp->add(std::move(ifLambda));
+    }
+
+    static auto addRegisterModification(const auto regAddress, const auto mode, const auto amount, Computer_* comp){
+        if(mode == "inc") return addRegisterIncrease(regAddress, amount, comp);
+        if(mode == "dec") return addRegisterDecrease(regAddress, amount, comp);
+    }
+
+    static auto addRegisterIncrease(const auto _regAddress, const auto _amount, Computer_* comp){
+        auto increase = [regAddress = _regAddress, amount = _amount, comp] () mutable {
+            comp->reg->operator[](regAddress)+=amount;
+            ++comp->currentInstructionPosition;
+        };
+        return comp->add(std::move(increase));
+    }
+
+    static auto addRegisterDecrease(const auto _regAddress, const auto _amount, Computer_* comp){
+        return addRegisterIncrease(_regAddress, -1*_amount, comp);
+    }
+
+
+    static auto make(const auto& instructions){
+        auto comp = Computer_{};
+        for(const auto& instr : instructions){
+            auto split = Utilities::split(instr);
+            addIf(split[5], split[4], std::stoi(split[6]), &comp);
+            addRegisterModification(split[0], split[1], std::stoi(split[2]), &comp);
+        }
+        return comp;
+    }
+};
 
 auto getHighestRegisterValues = [](auto& computer){
     auto logger = RegisterValueLogger{&computer};
@@ -167,7 +94,7 @@ auto getHighestRegisterValues = [](auto& computer){
 };
 
 int main(){
-    auto computer = parseInput(readFile::vectorOfStrings());
+    auto computer = ComputerFactory::make(readFile::vectorOfStrings());
 
     //Task 1
     const auto [maxRegisterValueAfterExecution, maxRegisterValueDuringExecution] = getHighestRegisterValues(computer);
